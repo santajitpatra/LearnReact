93a67ecbafa0f8983de48eeaa46ccad5c1f62508
Santajit Patra <business.santajitpatra@gmail.com>
2023-08-29 03:57:22 +0530

    learn : react router dom
    learn : react router dom

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,12 @@
+
+const Pagination = () => {
+  return (
+    <div>
+      <button>Prev</button>
+      <p>page of number</p>
+      <button>Next</button>
+    </div>
+  );
+};
+
+export default Pagination;
---
+++ src/routes/index.jsx
@@ -0,0 +1,13 @@
+export default function Index() {
+  return (
+    <p id="zero-state">
+      This is a demo for React Router created by Santajit Patra
+      <br />
+      Check out
+      <a href="https://santajitpatra.netlify.app/">
+        the docs santajitpatra.netlify.app
+      </a>
+      .
+    </p>
+  );
+}
